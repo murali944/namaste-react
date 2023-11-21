a51853f9cfdbe8312a8cf7775d8caaa9b05b1f79
pedapati murali <pedapati.murali@dish.com>
2023-11-21 18:33:55 +0530

    Added header element using JSX syntex

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-/*<div id="parent">
-    <div id="child">
-        <h1 id ="head1">Hello Murali !!</h1>
-        <h2 id ="head2">Hello Murali !!</h1>
-    </div>
-</div>
-*/
-const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" },
-    "Welcome to React by Namaste React !!"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-const pattern = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" },
-    [React.createElement("h1", { id: "head1" }, "Hello Murali !!"), React.createElement("h2", { id: "head2" }, "Hello Murali")]));
+const heading = (<h1 id="heading">Namaste React using JSX !!</h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(pattern);
+root.render(heading);
