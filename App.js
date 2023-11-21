fce63af1744f50d75d349de340521e215646a6d0
pedapati murali <pedapati.murali@dish.com>
2023-11-21 10:33:16 +0530

    Pushing codes to create child elements using createElement

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
+/*<div id="parent">
+    <div id="child">
+        <h1 id ="head1">Hello Murali !!</h1>
+        <h2 id ="head2">Hello Murali !!</h1>
+    </div>
+</div>
+*/
 const heading = React.createElement(
     "h1",
-    {id : "heading", xyz : "abc"},
+    { id: "heading", xyz: "abc" },
     "Welcome to React by Namaste React !!"
 );
+
+
+const pattern = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" },
+    [React.createElement("h1", { id: "head1" }, "Hello Murali !!"), React.createElement("h2", { id: "head2" }, "Hello Murali")]));
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(pattern);
