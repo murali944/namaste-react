93cd5e72f7d877a306082efb90d00f99aed67dcc
pedapati murali <pedapati.murali@dish.com>
2023-11-24 16:24:26 +0530

    Added card components to the application Added card components to the application

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1 id="heading">Namaste React using JSX !!</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const Header = () => <h2>Namaste React Header Component !!</h2>;
+
+const restaurentObject = [{ "id": 1, "restaurant": "Robena", "city": "Petrodvorets", "avatar": "https://robohash.org/voluptateminventorecum.png?size=50x50&set=set1" },
+{ "id": 2, "restaurant": "Jenda", "city": "Kapinango", "avatar": "https://robohash.org/ipsumoptionostrum.png?size=50x50&set=set1" },
+{ "id": 3, "restaurant": "Ashil", "city": "Mimbaan Timur", "avatar": "https://robohash.org/adremsint.png?size=50x50&set=set1" },
+{ "id": 4, "restaurant": "Rayshell", "city": "Murfreesboro", "avatar": "https://robohash.org/consequaturvoluptatemblanditiis.png?size=50x50&set=set1" },
+{ "id": 5, "restaurant": "Raleigh", "city": "Ząbkowice Śląskie", "avatar": "https://robohash.org/praesentiumipsumprovident.png?size=50x50&set=set1" },
+{ "id": 6, "restaurant": "Paige", "city": "Bulualto", "avatar": "https://robohash.org/atquisvelit.png?size=50x50&set=set1" },
+{ "id": 7, "restaurant": "Glen", "city": "Trubchevsk", "avatar": "https://robohash.org/nonetquaerat.png?size=50x50&set=set1" },
+{ "id": 8, "restaurant": "Vail", "city": "Rennes", "avatar": "https://robohash.org/nonautdolor.png?size=50x50&set=set1" },
+{ "id": 9, "restaurant": "Fitzgerald", "city": "Diónysos", "avatar": "https://robohash.org/remestsunt.png?size=50x50&set=set1" },
+{ "id": 10, "restaurant": "Jake", "city": "Xinjia", "avatar": "https://robohash.org/adipiscinonaut.png?size=50x50&set=set1" }]
 
 const HeaderComponent = () => {
   return (
@@ -20,12 +30,14 @@ const HeaderComponent = () => {
 
 }
 
-const RestaurantCard = ({resName,cusine}) => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
   return (
     <div className="restaurent-card">
-      <img src="https://seeklogo.com/images/Z/zomato-logo-01283E6B73-seeklogo.com.png"></img>
-      <h2>{resName}</h2>
-      <h2>{cusine}</h2>
+      <img className ="image-border" src={resData.avatar}></img>
+      <h3>{resData.restaurant}</h3>
+      <h4>{resData.city}</h4>
+
     </div>
 
   );
@@ -35,8 +47,7 @@ const BodyContainer = () => {
 
   return (
     <div className="restaurent-container">
-      <RestaurantCard resName="Angara" cusine="Biryani,Tandoori"/>
-      <RestaurantCard resName="Swagath" cusine="Biryani,Mutoon"/>
+      {restaurentObject.map((restaurant) => (<RestaurantCard key= {restaurant.id} resData={restaurant} />))}
 
     </div>
   );
@@ -57,11 +68,11 @@ const FooterComponent = () => {
 
 
 
-const Heading = () => (
+const Header = () => (
   <div id="container">
     <HeaderComponent />
     <BodyContainer />
     <FooterComponent />
   </div>
 );
-root.render(<Heading />);
+root.render(<Header />);
